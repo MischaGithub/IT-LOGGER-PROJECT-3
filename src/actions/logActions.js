0a6b7a0d39431f921c4d787191fe0ba11274b5be
MischaGithub <mischa.achmat120212@gmail.com>
2020-06-23 22:45:27 -1000

    Connecting Redux to a component

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -16,6 +16,7 @@ import { GET_LOGS, SET_LOADING, LOGS_ERROR } from "./types";
 //   };
 // };
 
+// Get logs from server
 export const getLogs = () => async (dispatch) => {
   // Refactored with some error handling
   try {
